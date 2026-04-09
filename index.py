c3a8c34c5b1929ec959ff123d98c2b4e907d1047
Pedro Rocha <jpedrorochac@gmail.com>
2026-04-09 19:00:01 -0300

    Aprendendo while

--- index.py
+++ index.py
@@ -1,20 +1,2 @@
-produto = input('Digite seu produto: ')
-preco = float(input('Digite o preço: '))
-quant = int(input('Digite a quantidade: '))
-
-if produto.strip() == "":
-    print('PRODUTO não pode ficar vazio')
-elif preco <= 0:
-    print('PREÇO não pode ser 0')
-elif quant <= 0:
-    print('QUANTIDADE não pode ser 0')
-else:
-    vt = preco * quant
-    print('---- Produto Cadastrado com Sucesso ----')
-    print(f'Seu produto é: {produto}')
-    print(f'Preço: R$ {preco:.2f}')
-    print(f'Quantidade: {quant}')
-    print(f'Valor total: R$ {vt:.2f}')
-
 
 
---
+++ while.py
@@ -0,0 +1,8 @@
+num = int( input ('Digite um número: '))
+soma = 0
+while num != 0  :
+    soma = soma + num
+    num = int( input ('Digite um número: '))
+
+
+print (f'A soma de todos os números digitados é: {soma}')
